203f1083e5164413da2a4cd145f668bcb6dfdcf8
deviprakash9908 <deviprakash9908@gmail.com>
2022-05-05 12:41:37 +0530

    Added questions

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,10 @@
 let questionAnswer = [{
-    question: "Sum of even numbers in the range 1 to 1000.",
+    question: "<h3>Sum of even numbers in the range 1 to 1000.</h3>",
     answer: "250500"
 },
 {
-    question: "question",
-    answer: "answer2"
+    question: "<h3>Sum of the strong numbers in the range 1 to 100000<br><h5>Strong numbers are the numbers which satisfy the below condition(the sum of the factorials of the digits is equal to the number itself).</h5><br><h5>145 = 1! + 4! + 5!</h5><br><h5>1! = 1<br>4!=24<br>5!=120<br>So the sum is 145</h5></h3>",
+    answer: "40733"
 },
 {
     question: "question",
@@ -36,31 +36,39 @@ let model_body = document.getElementById("model-body");
 
 let head_message, body_message;
 
+window.onload = function (params) {
+    question.innerHTML = questionAnswer[i].question;
+    document.location = "#section";
+}
 
 function validate() {
-if (submittedAnswer.value == questionAnswer[i].answer && i == 0) {
-    head_message = "Congratulations";
-    body_message = "You got The Answer. Now solve the second question";
-    i++;
-    question.innerText = "2. " + questionAnswer[i].question;
-    submittedAnswer.value = "";
-} else if (submittedAnswer.value == questionAnswer[i].answer && i == 1) {
-    head_message = "Congratulations";
-    body_message = "You got The Answer. Now solve the third question";
-    i++;
-    question.innerText = "3. " + questionAnswer[i].question;
-    submittedAnswer.value = "";
-} else if (submittedAnswer.value == questionAnswer[i].answer && i == 2) {
-    head_message = "Congratulations";
-    body_message = "The treasure is {KEY}";
-    submittedAnswer.value = "";
-} else {
-    head_message = "OOPS...";
-    body_message = "Wrong Answer Try Again";
-}
-model_head.innerText = head_message;
-model_body.innerText = body_message;
-modal.style.display = "block";
+    if (submittedAnswer.value == questionAnswer[i].answer && i == 0) {
+        head_message = "Congratulations";
+        body_message = "You got The Answer. Now solve the second question";
+        i++;
+        question.innerHTML = questionAnswer[i].question;
+        submittedAnswer.value = "";
+        document.querySelectorAll(".problem-1").forEach((element) => {element.style.color = "#FFF"});
+    } else if (submittedAnswer.value == questionAnswer[i].answer && i == 1) {
+        head_message = "Congratulations";
+        body_message = "You got The Answer. Now solve the third question";
+        i++;
+        question.innerText = "3. " + questionAnswer[i].question;
+        submittedAnswer.value = "";
+        document.querySelectorAll(".problem-2").forEach((element) => {element.style.color = "#FFF"});
+    } else if (submittedAnswer.value == questionAnswer[i].answer && i == 2) {
+        head_message = "Congratulations";
+        body_message = "The treasure is {KEY}";
+        submittedAnswer.value = "";
+        document.querySelectorAll(".problem-3").forEach((element) => {element.style.color = "#FFF"});
+    } else {
+        head_message = "OOPS...";
+        body_message = "Wrong Answer Try Again";
+    }
+    model_head.innerText = head_message;
+    model_body.innerText = body_message;
+    modal.style.display = "block";
+    document.location = "#solve";
 }
 
 
@@ -75,4 +83,26 @@ window.onclick = function (event) {
 if (event.target == modal) {
     modal.style.display = "none";
 }
+}
+
+document.addEventListener('contextmenu',(e)=>{
+    e.preventDefault();
+  }
+  );
+  document.onkeydown = function(e) {
+  if(event.keyCode == 123) {
+     return false;
+  }
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+     return false;
+  }
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
+     return false;
+  }
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+     return false;
+  }
+  if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+     return false;
+  }
 }
